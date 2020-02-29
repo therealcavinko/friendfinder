6cee9a731c14991b639f669c66753294b6b276eb
Calvin Cao <calvinknockout@gmail.com>
2020-02-29 11:03:39 -0500

    list of friends

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [{
+    "name" : "Tom",
+    "photo" : "https://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Rob",
+    "photo" : "https://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/GRO135948.png",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Devin",
+    "photo" : "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/13236.png",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Bill",
+    "photo" : "https://bostonglobe-prod.cdn.arcpublishing.com/resizer/Uv3nIFWNTnQWkYo1Z8e1OCLkCsk=/1440x0/arc-anglerfish-arc2-prod-bostonglobe.s3.amazonaws.com/public/BCQ526ANOUI6VK2QUEAUNBWRKU.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Julian",
+    "photo" : "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/12649.png",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Sony",
+    "photo" : "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3128721.png",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Vince",
+    "photo" : "https://img.bleacherreport.net/img/images/photos/003/688/165/hi-res-ae6718d3bb8560e2be113bbe470b305c_crop_north.jpg?1502299066&w=3072&h=2048",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}];
+
+module.exports = friends;
